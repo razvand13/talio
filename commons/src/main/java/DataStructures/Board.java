385c13d055d40ae5fd02d137d240cb7111582df7
Andrei Chiru <A.Chiru@student.tudelft.nl>
2023-03-08 10:41:43 +0100

    Data structures for the project

---
+++ commons/src/main/java/DataStructures/Board.java
@@ -0,0 +1,10 @@
+package DataStructures;
+
+import java.util.List;
+
+public class Board {
+    public String ID;
+    public String Title;
+    public List<List> Lists;
+    public String BgColor; //Background color
+}
---
+++ commons/src/main/java/DataStructures/List.java
@@ -0,0 +1,7 @@
+package DataStructures;
+
+public class List {
+    public String ID;
+    public String Name;
+    public java.util.List<Card> Cards;
+}
