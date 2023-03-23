8f7505406e58423bbe13d9d9dce566b5681a9123
jdemunck <j.c.demunck@student.tudelft.nl>
2023-03-23 16:51:08 +0100

    simplefy board api by removing methods now done by list or card api

--- server/src/main/java/server/api/BoardController.java
+++ server/src/main/java/server/api/BoardController.java
@@ -45,66 +45,12 @@ public class BoardController {
     }
 
     /**
-     * get a ListOfCards by its id and the id of the board in which it's located
-     * @param boardId id of the board in where the list is located
-     * @param listId id of the list we're looking for
-     * @return the list with the provided listId in the board with the provided boardId
-     */
-    @GetMapping("{boardId}/{listId}")
-    public ResponseEntity<ListOfCards> getListById(@PathVariable("boardId") long boardId, @PathVariable("listId") long listId){
-        if(listId<0 || boardId<0 || !repo.existsById(boardId)) {
-            return ResponseEntity.badRequest().build();
-        }
-        for(ListOfCards list: repo.getById(boardId).getLists()){
-            if(list.getId() == listId){
-                return ResponseEntity.ok(list);
-            }
-        }
-        return ResponseEntity.badRequest().build();
-    }
-
-    /**
-     * get a card by providing the ids of the card and of the board and list it's in.
-     * @param boardId the board the card is located in
-     * @param listId the list the card is located in
-     * @param cardId the card we're looking for
-     * @return badRequest iff the card couldn't be found, ok with Card iff it is found.
-     */
-    @GetMapping("{boardId}/{listId}/{cardId}")
-    public ResponseEntity<Card> getCardById(@PathVariable("boardId") long boardId, @PathVariable("listId") long listId, @PathVariable("cardId") long cardId){
-
-        if(cardId<0 || listId<0 || boardId<0 || !repo.existsById(boardId)){
-            return ResponseEntity.badRequest().build();
-        }
-
-        ListOfCards mylist = null;
-        for(ListOfCards list: repo.getById(boardId).getLists()){
-            if(list.getId() == listId){
-                mylist = list;
-            }
-        }
-        if(mylist == null){
-            return ResponseEntity.badRequest().build();
-        }
-
-
-        for(Card card: mylist.getCards()){
-            if(card.getId() == cardId){
-                return ResponseEntity.ok(card);
-            }
-        }
-        return ResponseEntity.badRequest().build();
-    }
-
-
-
-    /**
      *
-     * @param board the board that needs to be added
-     * @return badRequest iff the board is null. ok with the provided board iff it was added successfully.
+     * @param board the board to be added
+     * @return badRequest iff board is null or there already exists a board with the id of the provided board, ok(board) iff ith was added successfully
      */
     @PostMapping(path ={"","/"})
-    public ResponseEntity<Board> addBoard(@RequestBody Board board) {
+    public ResponseEntity<Board> add(@RequestBody Board board) {
 
         if(board == null){
             return ResponseEntity.badRequest().build();
@@ -118,42 +64,4 @@ public class BoardController {
         return ResponseEntity.ok(board);
     }
 
-    /**
-     * @param list the list that needs to be added
-     * @param id the id of the board it needs to be added to
-     * @return badRequest iff list is null or there exists no board with the provided id. ok with the provided list iff it was added successfully
-     */
-    @PostMapping("/{id}")
-    public ResponseEntity<ListOfCards> addList(@RequestBody ListOfCards list, @PathVariable("id") long id) {
-        if(list == null){
-            return ResponseEntity.badRequest().build();
-        }
-        if(id<0 || !repo.existsById(id)){
-            return  ResponseEntity.badRequest().build();
-        }
-
-        repo.findById(id).get().addList(list);
-        return ResponseEntity.ok(list);
-    }
-
-    /**
-     * adds a card to the provided list in the provided board
-     * @param card card that gets added
-     * @param boardId board it gets added to
-     * @param listId list it gets added to
-     * @return badrequest iff the provided  board or list couldn't be found. ok with card iff it was added successfully
-     */
-    @PostMapping("/{boardID}/{listId}")
-    public ResponseEntity<Card> addCard(@RequestBody Card card, @PathVariable("boardId") long boardId, @PathVariable("listId") long listId) {
-        if (card == null || listId<0 || boardId<0 || !repo.existsById(boardId)){
-            return ResponseEntity.badRequest().build();
-        }
-        for(ListOfCards list: repo.getById(boardId).getLists()){
-            if(list.getId() == listId){
-                list.addCard(card);
-                return ResponseEntity.ok(card);
-            }
-        }
-        return ResponseEntity.badRequest().build();
-    }
 }
