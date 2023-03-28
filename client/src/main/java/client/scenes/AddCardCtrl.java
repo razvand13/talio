de32d9e52d2815deeb21251d21892301acf90983
Petra Gulyas <p.gulyas-1@student.tudelft.nl>
2023-03-28 19:04:44 +0200

    Adding cards with DB, methods need to be implemented

---
+++ client/src/main/java/client/scenes/AddCardCtrl.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package client.scenes;
+
+import DataStructures.Card;
+import client.utils.ServerUtils;
+import com.google.inject.Inject;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class AddCardCtrl {
+
+    private final ServerUtils server;
+    private final MainTaskListCtrl mainCtrl;
+
+
+    @FXML
+    private TextField cardTitle;
+
+    @Inject
+    public AddCardCtrl(ServerUtils server, MainTaskListCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+
+    }
+
+    public void cancel() {
+        clearFields();
+        mainCtrl.showTaskListView();
+    }
+
+    public void ok() {
+
+        server.send("/app/cards", getCard());
+        clearFields();
+        mainCtrl.showTaskListView();
+    }
+
+    private Card getCard() {
+        var title = cardTitle.getText();
+        return new Card(title, null, null, null);
+    }
+
+    private void clearFields() {
+        cardTitle.clear();
+    }
+
+    public void keyPressed(KeyEvent e) {
+        switch (e.getCode()) {
+            case ENTER:
+                ok();
+                break;
+            case ESCAPE:
+                cancel();
+                break;
+            default:
+                break;
+        }
+    }
+}
--- client/src/main/java/client/scenes/MainTaskListCtrl.java
+++ client/src/main/java/client/scenes/MainTaskListCtrl.java
@@ -13,15 +13,21 @@ public class MainTaskListCtrl {
     private TaskListCtrl taskListCtrl;
     private Scene taskList;
 
+    private AddCardCtrl addCardCtrl;
+
+    private Scene addCard;
+
 
     /**Method for initializing main controller
      * @param primaryStage stage passed in as primary stage
      * @param taskList for setting up task list scene
      */
-    public void initialize(Stage primaryStage, Pair<TaskListCtrl, Parent> taskList) {
+    public void initialize(Stage primaryStage, Pair<TaskListCtrl, Parent> taskList, Pair<AddCardCtrl, Parent> addCard) {
         this.primaryStage = primaryStage;
         this.taskListCtrl = taskList.getKey();
         this.taskList = new Scene(taskList.getValue());
+        this.addCardCtrl = addCard.getKey();
+        this.addCard = new Scene(addCard.getValue());
 
         showTaskListView();
         primaryStage.show();
@@ -33,6 +39,12 @@ public class MainTaskListCtrl {
         taskListCtrl.firstTimeSetUp();
         primaryStage.setTitle("Task List");
         primaryStage.setScene(taskList);
+        taskListCtrl.refresh();
+    }
+
+    public void showAdd() {
+        primaryStage.setTitle("Add card");
+        primaryStage.setScene(addCard);
     }
 
 }
