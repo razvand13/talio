9f0ac714b89e1e19a025592c5371aaf098574666
razvand13 <R.D.Dinu-1@student.tudelft.nl>
2023-04-02 15:12:19 +0200

    Fixed pipeline, gradle builds locally

--- commons/src/test/java/commons/CardTest.java
+++ commons/src/test/java/commons/CardTest.java
@@ -3,8 +3,6 @@ package commons;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
