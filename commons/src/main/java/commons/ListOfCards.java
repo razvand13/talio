ff45eb9bb459e7a1cefeccbff5fe6f53b8fdfb56
Jan de Munck <j.c.demunck@student.tudelft.nl>
2023-04-02 16:24:04 +0200

    Some test for the api controllers

--- commons/src/main/java/commons/ListOfCards.java
+++ commons/src/main/java/commons/ListOfCards.java
@@ -5,6 +5,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 //import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 import javax.persistence.Entity;
@@ -12,61 +14,48 @@ import javax.persistence.Entity;
 //import org.apache.commons.lang3.builder.HashCodeBuilder;
 //import org.apache.commons.lang3.builder.ToStringBuilder;
 @Entity
-@Table(name="lists")
+@Table(name="list")
 public class ListOfCards implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(unique = true)
     public long id;
     public String name;
+
     @OneToMany(mappedBy = "list", fetch = FetchType.LAZY,
             cascade = CascadeType.ALL)
     private Set<Card> cards;
 
+
+
+    /*
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "board_id", nullable = false)
-    public Board board;
-
+    private Board board;
+*/
     @SuppressWarnings("unused")
-    private ListOfCards() {
+    public ListOfCards() {
         // for object mapper
     }
 
-    public ListOfCards(String name, Board board) {
+    public ListOfCards(String name/*, Board board*/) {
         this.name = name;
-        this.board = board;
+    //    this.board = board;
+        this.cards = new HashSet<>();
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         ListOfCards that = (ListOfCards) o;
-
-        if (id != that.id) return false;
-        if (!name.equals(that.name)) return false;
-        if (!cards.equals(that.cards)) return false;
-        return board.equals(that.board);
+        return id == that.id && Objects.equals(name, that.name);
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + name.hashCode();
-        result = 31 * result + cards.hashCode();
-        result = 31 * result + board.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "ListOfCards{" +
-                "id='" + id + '\'' +
-                ", name='" + name + '\'' +
-                ", cards=" + cards +
-                ", board=" + board +
-                '}';
+        return Objects.hash(id, name);
     }
 
     /**
@@ -81,6 +70,7 @@ public class ListOfCards implements Serializable {
      * simple getter
      * @return cards
      */
+
     public Set<Card> getCards() {
         return cards;
     }
---
+++ server/src/test/java/server/api/ListControllerTest.java
@@ -0,0 +1,96 @@
+package server.api;
+
+import commons.ListOfCards;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.OK;
+
+public class ListControllerTest {
+    TestListRepository listRepo;
+    ListController controller;
+
+    @BeforeEach
+    public void setup() {
+        listRepo = new TestListRepository();
+        controller = new ListController(listRepo);
+    }
+
+    @Test
+    public void cannotAddNullList() {
+        var actual = controller.add(null);
+        assertEquals(BAD_REQUEST, actual.getStatusCode());
+    }
+
+    @Test
+    public void canAddGoodList(){
+        var actual = controller.add(new ListOfCards("name"));
+        assertEquals(OK, actual.getStatusCode());
+    }
+
+    @Test
+    public void databaseIsUsed(){
+        controller.add(new ListOfCards("name"));
+        assertTrue(listRepo.calledMethods.contains("save"));
+    }
+
+    @Test
+    public void getAllTest() {
+        ListOfCards list0 = new ListOfCards("name");
+        ListOfCards list1 = new ListOfCards("name");
+        list0.id = 0;
+        list1.id = 1;
+        controller.add(list0);
+        controller.add(list1);
+
+        List expected = new ArrayList<ListOfCards>(Arrays.asList(list0, list1));
+
+        assertEquals(expected, controller.getAll());
+    }
+
+    @Test
+    public void getByIdTest() {
+        ListOfCards list0 = new ListOfCards("name");
+        list0.id = 0;
+        controller.add(list0);
+
+        assertEquals(list0, controller.getById(0).getBody());
+    }
+
+    @Test
+    public void deleteByIdTest() {
+        ListOfCards list0 = new ListOfCards("name");
+        ListOfCards list1 = new ListOfCards("name");
+        list0.id = 0;
+        list1.id = 1;
+        controller.add(list0);
+        controller.add(list1);
+
+        controller.deleteById(1);
+
+        assertEquals(BAD_REQUEST, controller.getById(1).getStatusCode());
+        assertEquals(OK, controller.getById(0).getStatusCode());
+    }
+
+    @Test
+    public void deleteAllTest() {
+        ListOfCards list0 = new ListOfCards("name");
+        ListOfCards list1 = new ListOfCards("name");
+        list0.id = 0;
+        list1.id = 1;
+        controller.add(list0);
+        controller.add(list1);
+
+        controller.deleteAll();
+
+        assertEquals(BAD_REQUEST, controller.getById(1).getStatusCode());
+        assertEquals(BAD_REQUEST, controller.getById(0).getStatusCode());
+    }
+}
