7527994717c57308b8e50a6c9ebe4d72f6f53887
jdemunck <j.c.demunck@student.tudelft.nl>
2023-04-02 16:45:40 +0200

    Test

---
+++ server/src/main/java/server/api/ListController.java
@@ -0,0 +1,122 @@
+package server.api;
+
+//import commons.Board;
+import commons.Card;
+import commons.ListOfCards;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.*;
+//import server.database.BoardRepository;
+import server.database.CardRepository;
+import server.database.ListRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/lists")
+public class ListController {
+    private ListRepository listRepo;
+    private CardRepository cardRepo;
+    //private BoardRepository boardRepo;
+
+    /**
+     *
+     * @param listRepo the list repository
+     * //@param boardRepo the board repository used when adding a list to see in which board to add it
+     */
+    public ListController(ListRepository listRepo/*, BoardRepository boardRepo*/) {
+        this.listRepo = listRepo;
+        //    this.boardRepo = boardRepo;
+    }
+
+    /**
+     *
+     * @return all lists in the repository
+     */
+    @GetMapping(path = {"", "/"})
+    public List<ListOfCards> getAll(){
+        return listRepo.findAll();
+    }
+
+    /**
+     *
+     * @param id id of the list you're looking for
+     * @return badRequest iff it wasn't found, ok with the list iff it was
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<ListOfCards> getById(@PathVariable("id") long id) {
+        if(id < 0 || !listRepo.existsById(id)) {
+            return  ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(listRepo.findById(id).get());
+    }
+
+    /**
+     *
+     * @param listOfCards the list that needs to be added
+     * //@param boardId the id of the board it needs to be added to
+     * @return badRequest iff it couldn't be added, ok with the list if it was added successfully
+     */
+    @PostMapping
+    public ResponseEntity<ListOfCards> add(@RequestBody ListOfCards listOfCards/*, long boardId*/){
+
+
+        if(listOfCards == null || listRepo.existsById(listOfCards.getId())){
+            return ResponseEntity.badRequest().build();
+        }
+
+/*
+        //check if the provided board exists
+        if(boardId<0 || !boardRepo.existsById(boardId)){
+            return ResponseEntity.badRequest().build();
+        }
+        Board boardWithID = boardRepo.getById(boardId);
+
+        boardWithID.addList(listOfCards);
+
+ */
+        listOfCards = listRepo.save(listOfCards);
+        return ResponseEntity.ok(listOfCards);
+    }
+
+    @PostMapping("/cards/{id}")
+    public ResponseEntity<Card> addCard(@PathVariable("id") long id, @RequestBody Card card){
+        System.out.println("got here");
+        if(id<0 || !listRepo.existsById(id)){
+            return ResponseEntity.badRequest().build();
+        }
+
+        //listRepo.getById(id).addCard(card);
+        cardRepo.save(card);
+        return ResponseEntity.ok(card);
+    }
+
+    @MessageMapping("/lists/cards") //app/quotes -> path for basically any client (consumer)
+    @SendTo("/topic/lists/cards")// (producer)
+    public Card addMessage(Card card) {
+        addCard(card.list.id, card);
+        return card;
+    }
+    @MessageMapping("/lists") //app/quotes -> path for basically any client (consumer)
+    @SendTo("/topic/lists")// (producer)
+    public ListOfCards addMessage(ListOfCards loc/*, long boardId*/) {
+        add(loc/*, boardId*/);
+        return loc;
+    }
+    /**
+     *
+     * @param id id of the list to be deleted
+     */
+    //@Transactional not sure if this is necessary
+    public void deleteById(long id){
+        listRepo.deleteById(id);
+    }
+    /**
+     * deletes all lists form the repo
+     */
+    public void deleteAll(){
+        listRepo.deleteAll();
+    }
+
+}
---
+++ server/src/test/java/server/api/BoardControllerTest.java
@@ -0,0 +1,94 @@
+//package server.api;
+//
+//import commons.Board;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.springframework.http.HttpStatus.BAD_REQUEST;
+//import static org.springframework.http.HttpStatus.OK;
+//
+//public class BoardControllerTest {
+//    TestBoardRepository repo;
+//    BoardController controller;
+//    @BeforeEach
+//    public void setup() {
+//        repo = new TestBoardRepository();
+//        controller = new BoardController(repo);
+//    }
+//
+//    @Test
+//    public void cannotAddNullBoard() {
+//        var actual = controller.add(null);
+//        assertEquals(BAD_REQUEST, actual.getStatusCode());
+//    }
+//    @Test
+//    public void canAddGoodBoard() {
+//        var actual = controller.add(new Board("name", "colour"));
+//        assertEquals(OK, actual.getStatusCode());
+//
+//    }
+//
+//    @Test
+//    public void databaseIsUsed() {
+//        controller.add(new Board("name", "colour"));
+//        assertTrue(repo.calledMethods.contains("save"));
+//    }
+//
+//    @Test
+//    public void getAllTest() {
+//        Board myBoard0 = new Board("name", "colour");
+//        myBoard0.id = 0;
+//        Board myBoard1 = new Board("name", "colour");
+//        myBoard1.id = 1;
+//        controller.add(myBoard0);
+//        controller.add(myBoard1);
+//
+//        List<Board> expected = new ArrayList<Board>(Arrays.asList(myBoard0,myBoard1));
+//
+//        assertEquals(expected, controller.getAll());
+//
+//    }
+//
+//    @Test
+//    public void getByIdTest() {
+//        Board myBoard = new Board("name", "colour");
+//        myBoard.id = 0;
+//        controller.add(myBoard);
+//        assertEquals(myBoard, controller.getById(0).getBody());
+//    }
+//
+//    @Test
+//    public void deleteByIdTest() {
+//        Board myBoard0 = new Board("name", "colour");
+//        myBoard0.id = 0;
+//        Board myBoard1 = new Board("name", "colour");
+//        myBoard1.id = 1;
+//        controller.add(myBoard0);
+//        controller.add(myBoard1);
+//
+//        controller.deleteById(1);
+//
+//        assertEquals(BAD_REQUEST, controller.getById(1).getStatusCode());
+//        assertEquals(OK, controller.getById(0).getStatusCode());
+//    }
+//
+//    @Test
+//    public void deleteAllTest() {
+//        Board myBoard0 = new Board("name", "colour");
+//        myBoard0.id = 0;
+//        Board myBoard1 = new Board("name", "colour");
+//        myBoard1.id = 1;
+//        controller.add(myBoard0);
+//        controller.add(myBoard1);
+//
+//        controller.deleteAll();
+//        assertEquals(BAD_REQUEST, controller.getById(0).getStatusCode());
+//        assertEquals(BAD_REQUEST, controller.getById(1).getStatusCode());
+//    }
+//}
