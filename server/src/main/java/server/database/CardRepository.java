681a191d69b8380a258c74ac601a442934479d65
razvand13 <R.D.Dinu-1@student.tudelft.nl>
2023-04-04 23:19:29 +0200

    Added Checkstyle

--- server/src/main/java/server/database/CardRepository.java
+++ server/src/main/java/server/database/CardRepository.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 public interface CardRepository extends JpaRepository<Card, Long>{
 
+    /**
+     * Find all Cards from the specified ListOfCards
+     * @param listId ListOfCards id
+     * @return a List<Card> containing the query result
+     */
     @Query("SELECT c FROM Card c WHERE c.listOfCards.id = :listId") // todo ORDER BY position
     List<Card> findAllByListId(long listId);
 }
