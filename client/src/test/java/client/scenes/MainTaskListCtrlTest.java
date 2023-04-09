c5291183dc7e292f7bb25e1ce4392970900b1ee0
Stephan Popov <S.K.Popov@student.tudelft.nl>
2023-04-09 10:07:19 +0200

    fixing checkstyle

--- client/src/test/java/client/scenes/MainTaskListCtrlTest.java
+++ client/src/test/java/client/scenes/MainTaskListCtrlTest.java
@@ -1,17 +1,17 @@
-package client.scenes;
-
-import client.utils.OurServerUtils;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
-
-
-class MainTaskListCtrlTest {
-
-
-
-}
+//package client.scenes;
+//
+//import client.utils.OurServerUtils;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//
+//
+//
+//class MainTaskListCtrlTest {
+//
+//
+//
+//}
