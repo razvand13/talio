0c4617cd10c96ce231aef0e7a1557d23d111c234
jdemunck <j.c.demunck@student.tudelft.nl>
2023-04-11 14:45:37 +0200

    oops, made a mistake merging

--- client/src/main/java/client/components/BoardContainer.java
+++ client/src/main/java/client/components/BoardContainer.java
@@ -7,6 +7,7 @@ import commons.Board;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
